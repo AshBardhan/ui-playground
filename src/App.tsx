8f2f648c4e4425275bbf3b95d0a53cb9ac833c63
AshBardhan <ashbardhan@gmail.com>
2025-07-01 12:38:10 +0530

    [filter-box] make use of 'react-router'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 import HomePage from './components/HomePage';
 import DynamicFilterPage from './components/DynamicFilterPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<main>
-				<HomePage />
-				<DynamicFilterPage />
-			</main>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/dynamic-filter" element={<DynamicFilterPage />} />
+			</Routes>
 		</div>
 	);
 }
