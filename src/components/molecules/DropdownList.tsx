86db05d67a8b6fbd0c3971609337bdf334d025e1
AshBardhan <ashbardhan@gmail.com>
2025-08-01 06:50:08 +0530

    [filter-box] some UI tweaks on 'dropdown-list' component via 'render-props'

--- src/components/molecules/DropdownList.tsx
+++ src/components/molecules/DropdownList.tsx
@@ -1,4 +1,6 @@
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react';
+import { ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
+import clsx from 'clsx';
 
 interface DropdownListProps {
 	options: string[];
@@ -8,20 +10,36 @@ interface DropdownListProps {
 }
 
 export const DropdownList = ({ options, disabled = false, selectedOption, onSelect }: DropdownListProps) => {
+	const listboxClasses = clsx(
+		'flex items-center justify-between gap-2 border px-2 rounded h-8 min-w-20',
+		disabled ? 'opacity-50 cursor-not-allowed bg-gray-100' : 'bg-white'
+	);
 	return (
 		<Listbox value={selectedOption} onChange={onSelect} disabled={disabled}>
-			<ListboxButton
-				className={`border px-2 py-1 rounded h-8 leading-none min-w-20 text-left ${disabled ? 'opacity-50 cursor-not-allowed bg-gray-100' : 'bg-white'}`}
-			>
-				{selectedOption}
-			</ListboxButton>
-			<ListboxOptions className="mt-1 bg-white border rounded shadow w-32 z-10" anchor="bottom">
-				{options.map((option) => (
-					<ListboxOption className="cursor-pointer px-2 py-1 hover:bg-gray-100" key={option} value={option}>
-						{option}
-					</ListboxOption>
-				))}
-			</ListboxOptions>
+			{({ open }) => (
+				<>
+					<ListboxButton className={listboxClasses}>
+						<span>{selectedOption}</span>
+						{open ? <ChevronUpIcon size={16} /> : <ChevronDownIcon size={16} />}
+					</ListboxButton>
+					<ListboxOptions className="mt-1 bg-white border rounded shadow w-32 z-10" anchor="bottom">
+						{options.map((option) => (
+							<ListboxOption key={option} value={option}>
+								{({ selected }) => (
+									<div
+										className={clsx(
+											'cursor-pointer px-2 py-1',
+											selected ? 'bg-blue-200 font-medium' : 'hover:bg-gray-100'
+										)}
+									>
+										{option}
+									</div>
+								)}
+							</ListboxOption>
+						))}
+					</ListboxOptions>
+				</>
+			)}
 		</Listbox>
 	);
 };
